8c4aa03f7fbed0b069a840cfc8c889485df3ccaa
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2024-12-18 16:14:33 +0100

    schema fix

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,29 +1,30 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: String,
+    isFavourite: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  email: String,
-  isFavourite: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    required: true,
-    default: 'personal',
-  },
-
-  // timestamps: true,
-});
+  { timestamps: true },
+);
 
 const ContactCollection = model('contact', contactSchema);
 export default ContactCollection;
