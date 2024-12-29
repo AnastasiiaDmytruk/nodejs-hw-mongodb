8275c69d8e720b882a3b3a5eef3be424d0d56675
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2024-12-29 22:20:01 +0100

    refactor: :art:

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -26,5 +26,5 @@ const contactSchema = new Schema(
   { versionKey: false, timestamps: true },
 );
 
-const ContactCollection = model('contact', contactSchema);
+const ContactCollection = model('contact', contactSchema); // хоча ми звертаємось до contacts в множині але тут пишемо contact тому що так історично склалось і монгуз сам знає що звертаємось до множини ( так в документаціі вказано)
 export default ContactCollection;
