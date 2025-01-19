e075fc210c8baaf43698b5e9944410797763dc57
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-01-19 19:38:01 +0100

    feat: :sparkles: add accessToken and refreshToken

--- src/controllers/controllers-auth.js
+++ src/controllers/controllers-auth.js
@@ -1,4 +1,4 @@
-import { register } from '../services/services-auth.js';
+import { register, login } from '../services/services-auth.js';
 
 export const registerController = async (req, res) => {
   await register(req.body);
@@ -6,5 +6,28 @@ export const registerController = async (req, res) => {
   res.status(201).json({
     status: 201,
     message: 'Successfully registered user',
+  });
+};
+
+export const loginController = async (req, res) => {
+  const session = await login(req.body);
+  console.log(session);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.cookie('sessionId', session.id, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully login user',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
