c2c6bdee989c427bd2e27ecf696676db0371ea38
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-01-23 14:22:53 +0100

    feat: :sparkles:

--- src/controllers/controllers-auth.js
+++ src/controllers/controllers-auth.js
@@ -1,4 +1,11 @@
-import { register, login, logout } from '../services/services-auth.js';
+import {
+  register,
+  login,
+  logout,
+  requestResetToken,
+} from '../services/services-auth.js';
+
+import { resetPassword } from '../services/services-auth.js';
 
 const setUpSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
@@ -59,4 +66,22 @@ export const logoutController = async (req, res) => {
   res.clearCookie('refreshToken');
   res.clearCookie('sessionId');
   res.status(204).send();
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
+  });
 };
--- src/services/services-auth.js
+++ src/services/services-auth.js
@@ -1,5 +1,6 @@
 import UserCollection from '../db/models/User.js';
 import SessionCollection from '../db/models/Session.js';
+import jwt from 'jsonwebtoken';
 
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
@@ -8,6 +9,14 @@ import {
   accessTokenLifeTime,
   refreshTokenLifeTime,
 } from '../constants/constants-user.js';
+
+import { getEnvVariable } from '../utils/getEnvVariable.js';
+import { sendEmail } from '../utils/sendEmail.js';
+import { SMTP, TEMPLATES_DIR } from '../constants/index.js';
+
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 const createSessionData = () => ({
   accessToken: randomBytes(30).toString('base64'),
@@ -76,6 +85,65 @@ export const logout = async (sessionId) => {
   await SessionCollection.deleteOne({ _id: sessionId });
 };
 
+export const requestResetToken = async (email) => {
+  const user = await UserCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    getEnvVariable('JWT_SECRET'),
+    { expiresIn: '15m' },
+  );
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
+  );
+
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.name,
+    link: `${getEnvVariable('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+  });
+
+  await sendEmail({
+    from: getEnvVariable(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset you  password',
+    html: `<p>Click <a href = "${resetToken}">here</a>to reset your password!</p>`,
+  });
+};
+
+export const resetPassword = async (payload) => {
+  let entries;
+  try {
+    entries = jwt.verify(payload.token, getEnvVariable('JWT_SECRET'));
+  } catch (err) {
+    if (err instanceof Error) throw createHttpError(401, err.message);
+
+    throw err;
+  }
+  const user = await UserCollection.findOne({
+    email: entries.email,
+    _id: entries.sub,
+  });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  await UserCollection.updateOne(
+    { _id: user._id },
+    { password: encryptedPassword },
+  );
+};
 export const getUser = (filter) => UserCollection.findOne(filter);
 
 export const getSession = (filter) => SessionCollection.findOne(filter);
